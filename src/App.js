1bb0693e62d68c04a4c3b9f201b996fd386aa0b5
aaryan1203 <aaryanjain1203@gmail.com>
2023-08-09 11:35:15 -0400

    Added microphone image

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="microphone"></div>
       <div className="next-text">NEXT</div>
-      <div className="outer-box"></div>
+      <div className="outer-box">
+        <h2 className="app-description">This app is about...</h2>
+      </div>
+      <div className="descriptions">
+        <div className="description description-1">Description 1</div>
+        <div className="description description-2">Description 2</div>
+        <div className="description description-3">Description 3</div>
+        <div className="description description-4">Description 4</div>
+        <div className="description description-5">Description 5</div>
+        <div className="description description-6">Description 6</div>
+      </div>
     </div>
   );
 }
